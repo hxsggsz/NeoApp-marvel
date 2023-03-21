ea91b5eeaca1086bfc92729caff37f7e814b92c6
Victor Hugo <vt.hugo.2021@gmail.com>
2023-03-20 22:42:04 -0300

    initial commit

---
+++ src/styles/GlobalStyle.ts
@@ -0,0 +1,36 @@
+import { createGlobalStyle } from 'styled-components';
+
+export const GlobalStyle = createGlobalStyle`
+   :root {
+    font-size: 62.5%; // 62.5% = 10px
+    --red: #F00001;
+    --white: #fff;
+    --black: #000;
+    --dark-grey: #1e1e1e;
+  }
+
+   * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+
+  body {
+    font-family: sans-serif;
+      width: 100vw;
+  }
+  
+  html {
+    display: flex; 
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    min-height: 100%;
+    overflow-x: hidden;
+  }
+
+  body {
+    display: flex;
+    flex: 1;
+  }
+`;
