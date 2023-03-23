75e8861da5af870d9e327aef5953326237c453f6
victorhugo-js <vt.hugo.2021@gmail.com>
2023-03-23 15:44:26 -0300

    fixed: header image not centralized on mobile

--- src/components/header/index.ts
+++ src/components/header/index.ts
@@ -1,8 +1,17 @@
 import styled from 'styled-components';
 
 export const StyledHeader = styled.div`
-  img {
-    width: 100vw;
-    height: 100vh;
-  }
+display: flex;
+align-items: start;
+justify-content: center;
+width: 100vw;
+height: 100vh;
+img {
+  /* a imagem nao tem o texto centralizado :P */
+    margin-right: 8rem;
+    width: auto;
+    height: auto;
+    object-fit: scale-down;
+    /* object-position: center; */
+    }
 `;
